b40cd21423c22d3cb5b864b90a6e376429de0ac4
Roman van Gemmeren <roman.van-gemmeren@chefkoch.de>
2017-10-03 16:04:39 +0200

    + doc, envsubst config.php

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+# replace variables in ttrss-config
+envsubst < /var/www/ttrss/config.php-dist > /var/www/ttrss/config.php
+chown nginx:nginx /var/www/ttrss/config.php
+
 # Start supervisord
 echo "Starting supervisord"
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
